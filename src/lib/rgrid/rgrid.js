b4f021c66db3489df0c70382fe9a55b9de99e25b
boyetp123 <boyetp@yahoo.com>
2018-07-08 22:02:00 -0700

    callback on delay dataload working now

--- src/lib/rgrid/rgrid.js
+++ src/lib/rgrid/rgrid.js
@@ -1,6 +1,8 @@
-import React, { Component, ReactDOM } from 'react';
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import RGridHeaderCell from './rgridHeaderCell';
 import RGridDataCell from './rgridDataCell';
+import _ from 'lodash';
 // import { ColumnDef, SortClasses, GridHdrClasses, HAlignmentClasses } from './rgridDefs';
 // import moment  from 'moment';
 // import numeral from 'numeraljs';
@@ -10,6 +12,9 @@ export default class RGrid extends Component {
     constructor(props) {
         super(props);
         this.defaultBodyCellPresenter = RGridDataCell;
+        this.centerBodyUid = _.uniqueId('cbody-')
+        this.leftBodyUid = _.uniqueId('lbody-')
+        this.rightBodyUid = _.uniqueId('rbodybody-')
     }
     renderBodyRow( colDefs ) {
         if (!colDefs || !this.props.gridOptions.rowData) return null;
@@ -42,17 +47,17 @@ export default class RGrid extends Component {
                     <tbody>
                         <tr>
                         <td className="left-pane" style={{display:"none"}}>
-                            <div className="mygrid-left">
+                            <div className="mygrid-left" id={this.leftBodyUid}>
                                 {this.renderBodyRow( null )}
                             </div>
                         </td>
                         <td className="center-pane">
-                            <div className="mygrid-center">
+                            <div className="mygrid-center" id={this.centerBodyUid}>
                                 {this.renderBodyRow(this.props.gridOptions.columnDefs)}
                             </div>
                         </td>
                         <td className="right-pane" style={{display:"none"}}>
-                            <div className="mygrid-right">
+                            <div className="mygrid-right" id={this.rightBodyUid}>
                                 {this.renderBodyRow( null )}
                             </div>
                         </td>
@@ -130,7 +135,10 @@ export default class RGrid extends Component {
 
     setData(data) {
         this.props.gridOptions.rowData = data;
-        this.renderBodyRow(this.props.gridOptions.columnDefs);
+        ReactDOM.render(this.renderBodyRow(this.props.gridOptions.columnDefs),
+            document.getElementById(this.centerBodyUid)
+        )
+        ;
     }
 
 }
